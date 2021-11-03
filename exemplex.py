7675d79baaa8f49d8d3e77a61c85112610d6a3d9
Gheorghițanu Bianca-Gabriela <79106145+biancaggh@users.noreply.github.com>
2021-11-03 22:03:23 +0200

    Update exemplex.py

--- exemplex.py
+++ exemplex.py
@@ -31,7 +31,7 @@ def load_planar_dataset():
     Y = np.zeros((m,1), dtype='uint8') # labels vector (0 for red, 1 for blue)
     a = 4 # maximum ray of the flower
 
-    for j in range(2):
+    for j in range(3):
         ix = range(N*j,N*(j+1))
         t = np.linspace(j*3.12,(j+1)*3.12,N) + np.random.randn(N)*0.2 # theta
         r = a*np.sin(4*t) + np.random.randn(N)*0.2 # radius
